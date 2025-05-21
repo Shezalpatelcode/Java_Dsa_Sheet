ad276101e82ca17bcb1d4a25da5813e87f346815
Shezalpatelcode <sheezalp88@gmail.com>
2025-05-22 00:37:40 +0530

    Java problems

--- code4.java
+++ code4.java
@@ -1,5 +1,49 @@
-public class code4 {
-    public static void main(String args[]){
-           
+ public class code4{
+ public void sortArray(int a[]){
+     int low,mid,high;
+     low=mid=0;
+     high=a.length-1;
+
+     while(mid<=high){
+         switch(a[mid]){
+            //Case when you encounter 0;
+            case 0:
+              if(a[low]!=a[mid]){
+                 int t=a[low];
+                 a[low]=a[mid];
+                 a[mid]= t;
+
+              }
+              low++;
+              mid++;
+              break;
+              //Case when you encounter 1;
+              case 1:
+                 mid++;
+                 break;
+                 //Case when you encounter 2;
+              case 2:
+                 if(a[mid]!=a[high]){
+                 int t=a[mid];
+                 a[mid]=a[high];
+                 a[high]=t;
+              }
+              high--;
+         }
+     }
+     System.out.println("----Dutch National Flag Printing--->");
+     for(int i=0;i<a.length-1;i++){
+        System.out.print(a[i]+ " ");
+     }
+     System.out.println();
+     System.out.println("-------------------------------");
+ }
+ 
+    public static void main(String[] args)
+    {
+       code4 obj= new code4();
+       int a[]={0,1,1,0,2,2,1,0,2};
+       obj.sortArray(a);
+
     }
-}
+ }
--- code5.java
+++ code5.java
@@ -1,3 +1,5 @@
+//Q) move all the negative numbers to beggining and postive number to the end of the array
+//----------------------------------------------------
 public class code5 {
     void movenum(int a[])
     {
@@ -12,16 +14,16 @@ public class code5 {
                    j++;
                 }
             }
-        
-        System.out.println("Array after arrangment--->");
+          System.out.println("Array after arrangment--->");
         for(int i=0;i<a.length-1;i++){
             System.out.print(a[i]+ " ");
         }
     }
-    
+    //-----------------------------------------
     public static void main(String args[]){
          int a[]={2,3,-4,-78,76,100};
          code5 obj= new code5();
          obj.movenum(a);
     }
 }
+//-----------------------------------------------
