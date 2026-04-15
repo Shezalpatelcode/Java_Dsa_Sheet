6bbfdeff9f845f086d555dd350d9501783c8f073
Shezalpatelcode <sheezalp88@gmail.com>
2026-04-15 23:21:17 +0530

    dsa

--- 136lc.java
+++ 136lc.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result =0;
         for(int num: nums){
-            result^=num;
+            result^=num;//XOR done to all the numbers of the array
         }
         return result;
     }
