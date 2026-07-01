b3264f8673f633301e02d9ff1d0f71982fb4d7f6
Shezalpatelcode <sheezalp88@gmail.com>
2026-07-01 23:23:34 +0530

    2721 Leetcode problem

---
+++ 2721lc.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Function[]} functions
+ * @return {Promise<any[]>}
+ */
+var promiseAll = function(functions) {
+
+    return new Promise((resolve, reject) => {
+
+        const results = [];
+        let completed = 0;
+
+        for (let i = 0; i < functions.length; i++) {
+
+            functions[i]()
+                .then(value => {
+
+                    results[i] = value;
+                    completed++;
+
+                    if (completed === functions.length) {
+                        resolve(results);
+                    }
+
+                })
+                .catch(error => {
+                    reject(error);
+                });
+
+        }
+
+    });
+
+};
